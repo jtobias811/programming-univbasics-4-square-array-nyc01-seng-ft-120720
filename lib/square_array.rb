fd5325f3f51be58bd33805a01c48574a4a2194b1
Jacob Tobias <jtobias811@gmail.com>
2020-11-01 16:57:51 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,6 @@
 def square_array(numbers)
   new_array = []
-  while square_array.length do |numbers|
+  while square_array.length do
     new_array.push(array(numbers * numbers))
     count += 1
   end
